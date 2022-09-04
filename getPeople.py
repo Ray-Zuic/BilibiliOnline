db9bd8e0f1d1c0bd295c8dc661177fd44330945b
PPh0914 <461098453@qq.com>
2022-09-04 18:57:02 +0800

    思路

--- getPeople.py
+++ getPeople.py
@@ -13,3 +13,10 @@ def get_html(url):
 #https://api.bilibili.com/x/player/online/total?aid=260132618&cid=822823845&bvid=BV1ie411g72J&ts=55408594
 #需要aid,cid,bvid
 
+#该方法用于获取当天热门前10的视频
+
+#根据视频的信息查找对应的完整信息，并存入
+
+#https://api.bilibili.com/x/player/v2?aid=260132618&cid=822823845该接口包含视频和访问端信息
+#或者通过播放量的差值来计算，播放量数据来自页面本身，
+#查询出来的数据存入excel文件
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from getPeople import get_html
 
 if __name__ == '__main__':
+    #此url为视频详情页的地址
     url = "https://api.bilibili.com/x/player/online/total"
     print(get_html(url))
